eca74147d859ae090685a1d61e1e6492c7a601c0
genrator-lgtm <kupasva663@gmail.com>
2025-07-21 22:28:40 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://meetup-instance.onrender.com',
+        ws: true,
+        changeOrigin: true
+      },
+      '/api': {
+        target: 'https://meetup-instance.onrender.com',
+        changeOrigin: true
+      }
+    },
+  }
+})
